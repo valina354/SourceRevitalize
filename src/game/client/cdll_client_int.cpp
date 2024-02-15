6fa9f789f90a7d19eb7ee93f01ff57ccae98428b
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2024-02-15 14:46:45 +0200

    Add error message incase hardware doesnt support shader model 3

--- src/game/client/cdll_client_int.cpp
+++ src/game/client/cdll_client_int.cpp
@@ -124,6 +124,11 @@
 #include "sourcevr/isourcevirtualreality.h"
 #include "client_virtualreality.h"
 #include "mumble.h"
+#include <cstdlib> // for exit
+#include <cstdio>  // for printf
+
+// Declare MessageBox function signature
+extern "C" __declspec( dllimport ) int __stdcall MessageBoxA( void *hWnd, const char *lpText, const char *lpCaption, unsigned int uType );
 
 // NVNT includes
 #include "hud_macros.h"
@@ -1125,6 +1130,13 @@ int CHLClient::Init( CreateInterfaceFn appSystemFactory, CreateInterfaceFn physi
 	IGameSystem::Add( ClientSoundscapeSystem() );
 	IGameSystem::Add( PerfVisualBenchmark() );
 	IGameSystem::Add( MumbleSystem() );
+
+    // Don't want Source engine running on unsupported hardware
+	if ( g_pMaterialSystemHardwareConfig->GetDXSupportLevel() < 95 )
+	{
+		MessageBoxA( nullptr, "This game has a minimum requirement of Shader Model 3.0 to run", "Engine Error GPU", 0x10 /* MB_ICONERROR */ );
+		exit( 0 );
+	}
 	
 	#if defined( TF_CLIENT_DLL )
 	IGameSystem::Add( CustomTextureToolCacheGameSystem() );
