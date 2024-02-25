c2cb9bb607bd147e9a1b7535202c8fdba953f0cf
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2024-02-25 14:39:46 +0200

    reupload all the code
    cuz i had to redownload it to fix something

--- src/materialsystem/stdshaders/LightmappedPBR_dx9.cpp
+++ src/materialsystem/stdshaders/LightmappedPBR_dx9.cpp
@@ -17,8 +17,6 @@ BEGIN_VS_SHADER(LightmappedPBR,
 	"Help for LightmappedPBR")
 #endif
 
-
-
 BEGIN_SHADER_PARAMS
 SHADER_PARAM(ALPHATESTREFERENCE, SHADER_PARAM_TYPE_FLOAT, "0.0", "")
 SHADER_PARAM(ENVMAP, SHADER_PARAM_TYPE_TEXTURE, "shadertest/shadertest_env", "envmap")
@@ -90,9 +88,6 @@ void SetupVars(DrawLightPass_Vars_t& info)
 
 SHADER_INIT_PARAMS()
 {
-	// PBR relies heavily on envmaps
-	if ( !params[ENVMAP]->IsDefined() )
-		params[ENVMAP]->SetStringValue( "env_cubemap" );
 	LightmappedPBR_DX9_Vars_t info;
 	SetupVars(info);
 	InitParamsLightmappedPBR_DX9(this, params, pMaterialName, info);
@@ -100,8 +95,6 @@ SHADER_INIT_PARAMS()
 
 SHADER_FALLBACK
 {
-	if ( !g_pHardwareConfig->SupportsShaderModel_3_0() )
-		return "SDK_VertexLitGeneric";
 	return 0;
 }
 
@@ -132,4 +125,3 @@ SHADER_DRAW
 }
 
 END_SHADER
-
