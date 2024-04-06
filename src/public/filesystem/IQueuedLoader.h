7b7bd21b8ffddcef4ced1fea6293f904642d24b3
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2024-04-06 20:33:56 +0300

    add gameui from leaks,some studio stuff and more

--- src/public/filesystem/IQueuedLoader.h
+++ src/public/filesystem/IQueuedLoader.h
@@ -1,6 +1,6 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose:
 //
 //===========================================================================//
 
@@ -8,7 +8,7 @@
 #define QUEUEDLOADER_H
 
 #ifdef _WIN32
-#pragma once
+	#pragma once
 #endif
 
 #include "tier0/platform.h"
@@ -18,16 +18,16 @@ class CFunctor;
 
 enum LoaderError_t
 {
-	LOADERERROR_NONE     = 0,
+	LOADERERROR_NONE = 0,
 	LOADERERROR_FILEOPEN = -1,
-	LOADERERROR_READING  = -2,
+	LOADERERROR_READING = -2,
 };
 
 enum LoaderPriority_t
 {
-	LOADERPRIORITY_ANYTIME    = 0,		// low priority, job can finish during gameplay
-	LOADERPRIORITY_BEFOREPLAY = 1,		// job must complete before load ends
-	LOADERPRIORITY_DURINGPRELOAD = 2,	// job must be complete during preload phase
+	LOADERPRIORITY_ANYTIME = 0,		  // low priority, job can finish during gameplay
+	LOADERPRIORITY_BEFOREPLAY = 1,	  // job must complete before load ends
+	LOADERPRIORITY_DURINGPRELOAD = 2, // job must be complete during preload phase
 };
 
 typedef void ( *QueuedLoaderCallback_t )( void *pContext, void *pContext2, const void *pData, int nSize, LoaderError_t loaderError );
@@ -41,16 +41,16 @@ struct LoaderJob_t
 		memset( this, 0, sizeof( *this ) );
 	}
 
-	const char				*m_pFilename;				// path to resource
-	const char				*m_pPathID;					// optional, can be NULL
-	QueuedLoaderCallback_t	m_pCallback;				// called at i/o delivery
-	void					*m_pContext;				// caller provided data
-	void					*m_pContext2;				// caller provided data
-	void					*m_pTargetData;				// optional, caller provided target buffer
-	int						m_nBytesToRead;				// optional read clamp, otherwise 0
-	unsigned int			m_nStartOffset;				// optional start offset, otherwise 0
-	LoaderPriority_t		m_Priority;					// data must arrive by specified interval
-	bool					m_bPersistTargetData;		// caller wants ownership of i/o buffer
+	const char *m_pFilename;			// path to resource
+	const char *m_pPathID;				// optional, can be NULL
+	QueuedLoaderCallback_t m_pCallback; // called at i/o delivery
+	void *m_pContext;					// caller provided data
+	void *m_pContext2;					// caller provided data
+	void *m_pTargetData;				// optional, caller provided target buffer
+	int m_nBytesToRead;					// optional read clamp, otherwise 0
+	unsigned int m_nStartOffset;		// optional start offset, otherwise 0
+	LoaderPriority_t m_Priority;		// data must arrive by specified interval
+	bool m_bPersistTargetData;			// caller wants ownership of i/o buffer
 };
 
 enum ResourcePreload_t
@@ -88,9 +88,15 @@ public:
 // Default implementation
 class CResourcePreload : public IResourcePreload
 {
-	void PurgeUnreferencedResources()	{}
-	void OnEndMapLoading( bool bAbort )	{}
-	void PurgeAll() {}
+	void PurgeUnreferencedResources()
+	{
+	}
+	void OnEndMapLoading( bool bAbort )
+	{
+	}
+	void PurgeAll()
+	{
+	}
 };
 
 // UI can install progress notification
@@ -104,51 +110,53 @@ public:
 };
 
 // spew detail
-#define LOADER_DETAIL_NONE				0
-#define LOADER_DETAIL_TIMING			(1<<0)
-#define LOADER_DETAIL_COMPLETIONS		(1<<1)
-#define LOADER_DETAIL_LATECOMPLETIONS	(1<<2)
-#define LOADER_DETAIL_PURGES			(1<<3)
+#define LOADER_DETAIL_NONE 0
+#define LOADER_DETAIL_TIMING ( 1 << 0 )
+#define LOADER_DETAIL_COMPLETIONS ( 1 << 1 )
+#define LOADER_DETAIL_LATECOMPLETIONS ( 1 << 2 )
+#define LOADER_DETAIL_PURGES ( 1 << 3 )
 
-#define QUEUEDLOADER_INTERFACE_VERSION		"QueuedLoaderVersion004"
+#define QUEUEDLOADER_INTERFACE_VERSION "QueuedLoaderVersion004"
 abstract_class IQueuedLoader : public IAppSystem
 {
 public:
-	virtual void				InstallLoader( ResourcePreload_t type, IResourcePreload *pLoader ) = 0;
-	virtual void				InstallProgress( ILoaderProgress *pProgress ) = 0;
+	virtual void InstallLoader( ResourcePreload_t type, IResourcePreload * pLoader ) = 0;
+	virtual void InstallProgress( ILoaderProgress * pProgress ) = 0;
 
 	// Set bOptimizeReload if you want appropriate data (such as static prop lighting)
 	// to persist - rather than being purged and reloaded - when going from map A to map A.
-	virtual bool				BeginMapLoading( const char *pMapName, bool bLoadForHDR, bool bOptimizeMapReload ) = 0;
-	virtual void				EndMapLoading( bool bAbort ) = 0;
-	virtual bool				AddJob( const LoaderJob_t *pLoaderJob ) = 0;
+	virtual bool BeginMapLoading( const char *pMapName, bool bLoadForHDR, bool bOptimizeMapReload ) = 0;
+	virtual void EndMapLoading( bool bAbort ) = 0;
+	virtual bool AddJob( const LoaderJob_t *pLoaderJob ) = 0;
 
 	// injects a resource into the map's reslist, rejected if not understood
-	virtual void				AddMapResource( const char *pFilename ) = 0;
+	virtual void AddMapResource( const char *pFilename ) = 0;
 
 	// dynamically load a map resource
-	virtual void				DynamicLoadMapResource( const char *pFilename, DynamicResourceCallback_t pCallback, void *pContext, void *pContext2 ) = 0;
-	virtual void				QueueDynamicLoadFunctor( CFunctor* pFunctor ) = 0;
-	virtual bool				CompleteDynamicLoad() = 0;
+	virtual void DynamicLoadMapResource( const char *pFilename, DynamicResourceCallback_t pCallback, void *pContext, void *pContext2 ) = 0;
+	virtual void QueueDynamicLoadFunctor( CFunctor * pFunctor ) = 0;
+	virtual bool CompleteDynamicLoad() = 0;
+	virtual void QueueCleanupDynamicLoadFunctor( CFunctor * pFunctor ) = 0;
+	virtual bool CleanupDynamicLoad() = 0;
 
 	// callback is asynchronous
-	virtual bool				ClaimAnonymousJob( const char *pFilename, QueuedLoaderCallback_t pCallback, void *pContext, void *pContext2 = NULL ) = 0;
+	virtual bool ClaimAnonymousJob( const char *pFilename, QueuedLoaderCallback_t pCallback, void *pContext, void *pContext2 = NULL ) = 0;
 	// provides data if loaded, caller owns data
-	virtual bool				ClaimAnonymousJob( const char *pFilename, void **pData, int *pDataSize, LoaderError_t *pError = NULL ) = 0;
+	virtual bool ClaimAnonymousJob( const char *pFilename, void **pData, int *pDataSize, LoaderError_t *pError = NULL ) = 0;
 
-	virtual bool				IsMapLoading() const = 0;
-	virtual bool				IsSameMapLoading() const = 0;
-	virtual bool				IsFinished() const = 0;
+	virtual bool IsMapLoading() const = 0;
+	virtual bool IsSameMapLoading() const = 0;
+	virtual bool IsFinished() const = 0;
 
 	// callers can expect that jobs are not immediately started when batching
-	virtual bool				IsBatching() const = 0;
+	virtual bool IsBatching() const = 0;
 
-	virtual bool				IsDynamic() const = 0;
+	virtual bool IsDynamic() const = 0;
 
 	// callers can conditionalize operational spew
-	virtual int					GetSpewDetail() const = 0;
+	virtual int GetSpewDetail() const = 0;
 
-	virtual void				PurgeAll() = 0;
+	virtual void PurgeAll() = 0;
 };
 
 extern IQueuedLoader *g_pQueuedLoader;
